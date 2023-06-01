e48de2ba5778df586b0b7414278f3eac74383576
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-05-31 21:49:28 -0500

    Agregando localstorage

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,7 +3,7 @@
 
 //! OBTENER DATOS DESDE EL DOM
 
-//*Variables 
+//*Variables
 
 let cantCajas = 0;
 let pesoCaja = 0;
@@ -14,6 +14,7 @@ let costoTotal = 0;
 let resultado = 0;
 let totalEnvio = 0;
 let nombreCiudad = 0;
+let costoCiudad = 0;
 
 //?---- FUNCION TOMAR DATOS DE LOS INPUT------
 //Se toma del html, con el id, el elemento input donde se ingresa la cantidad de cajas y peso de cada caja.
@@ -47,42 +48,50 @@ function getSelectorCiudad() {
 
 getSelectorCiudad();
 
-
 //?---- FUNCION PESO TOTAL------
-//Cantidad de cajas x peso de cada una. 
+//Cantidad de cajas x peso de cada una.
 
-
-function pesoTotalFlete(cantCajas,pesoCaja) {
-
-  console.log("Entramos a peso total:" + cantCajas + " y " +  pesoCaja);
+function pesoTotalFlete(cantCajas, pesoCaja) {
+  console.log("Entramos a peso total:" + cantCajas + " y " + pesoCaja);
   pesoTotal = cantCajas * pesoCaja;
   console.log("Peso total es: " + pesoTotal);
   return pesoTotal;
 }
 
-pesoTotalFlete(cantCajas,pesoCaja);
-
+pesoTotalFlete(cantCajas, pesoCaja);
 
 //?---- FUNCION COSTO CIUDAD------
 
 //!LISTADO DE CIUDADES Y PRECIO POR KG
 //Cada ciudad tiene su precio por kilogramo.
 
-const costoCiudad = [
-  {indicador: '1', ciudad: 'Medellin', costo: 895},
-  {indicador: '2', ciudad: 'Cali', costo: 494},
-  {indicador: '3', ciudad: 'Pereira', costo: 795},
-  {indicador: '4', ciudad: 'Barranquilla', costo: 1250},
-];
 
+guardarLocalStorage();
+//Guardo el listado de ciudades con sus respectivos precios en un array de objetos en el localstorage.
+function guardarLocalStorage() {
+  costoCiudad = [
+    { indicador: "1", ciudad: "Medellin", costo: 895 },
+    { indicador: "2", ciudad: "Cali", costo: 494 },
+    { indicador: "3", ciudad: "Pereira", costo: 795 },
+    { indicador: "4", ciudad: "Barranquilla", costo: 1250 },
+  ];
+  //convierto en texto JSON
+  localStorage.setItem("costoCiudad", JSON.stringify(costoCiudad));
+}
 
-function getCostoCiudad(selector) {
+obtenerLocalStorage();
+//obtengo los datos guardados inicialmente y los convierto en un array nuevamente
+function obtenerLocalStorage() {
+  let costoCiudad = JSON.parse(localStorage.getItem("costoCiudad"));
+  return costoCiudad;
+}
+
+function getCostoCiudad(selector, costoCiudad) {
   resultado = costoCiudad.find((costo) => costo.indicador === selector);
   resultado = resultado.costo;
+  console.log(resultado);
   return resultado;
-};
-
-getCostoCiudad(selector);
+}
 
 //?---- IDENTIFICAR CIUDAD------
 
@@ -94,11 +103,9 @@ function identificarCiudad() {
   console.log("ciudad: " + nombreCiudad);
 
   return nombreCiudad;
-
-};
+}
 
 identificarCiudad();
-
 
 //?---- FUNCION COSTO ENVIO------
 //peso total x $ kilogramo de acuerdo a la ciudad
@@ -118,23 +125,18 @@ function costoEnvio(pesoTotal, resultado) {
   return totalEnvio;
 }
 
-costoEnvio(pesoTotal,resultado);
+costoEnvio(pesoTotal, resultado);
 
-
-function cotizacionFinal(totalEnvio) {
-
-};
+function cotizacionFinal(totalEnvio) {}
 
 cotizacionFinal(totalEnvio);
-
 
 //?---- CREAR RESULTADO------
 //Se tomaran los datos de cantidad de cajas, la ciudad a la que se enviara el pedido y el total cotizado para ser reflejados en el html por medio de la manipulacion del DOM
 
-
-function crearCotizacion(cantCajas,nombreCiudad,totalEnvio) {
+function crearCotizacion(cantCajas, nombreCiudad, totalEnvio) {
   // document.querySelector(".button").addEventListener("click", (e) => {
-  if(cantCajas > 0) {
+  if (cantCajas > 0) {
     const newElement = document.createElement("div");
     newElement.classList.add("div");
     newElement.innerHTML = `
@@ -150,19 +152,17 @@ function crearCotizacion(cantCajas,nombreCiudad,totalEnvio) {
     document.querySelector(".resultado").appendChild(newElement);
   } else return [];
   // });
-};
+}
 
-crearCotizacion(cantCajas,nombreCiudad,totalEnvio);
-
-
+crearCotizacion(cantCajas, nombreCiudad, totalEnvio);
 
 function ejecucion() {
   getDatos();
   getSelectorCiudad();
   pesoTotalFlete(cantCajas, pesoCaja);
-  getCostoCiudad(selector);
+  getCostoCiudad(selector, costoCiudad);
   identificarCiudad();
-  costoEnvio(pesoTotal,resultado);
+  costoEnvio(pesoTotal, resultado);
   cotizacionFinal(totalEnvio);
-  crearCotizacion(cantCajas,nombreCiudad,totalEnvio);
-};
+  crearCotizacion(cantCajas, nombreCiudad, totalEnvio);
+}
