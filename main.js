dcc05c54fc58fa66ff3bdfa34edfdd76a5b4503d
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-06-16 14:55:17 +0000

    otro

--- main.js
+++ main.js
@@ -1,73 +1,70 @@
-
 //*Variables
-
 
 let cantCajas = 0;
 let pesoCaja = 0;
-let anchoCaja = 0; 
-let largoCaja = 0; 
+let anchoCaja = 0;
+let largoCaja = 0;
 let altoCaja = 0;
 let pesoVol = 0;
 let pesoTotalFlete = 0;
-
-
 
 //! INPUTS DE USUARIO
 //Datos que registra el usuario necesarios para hacer los calculos de peso neto y peso volumen.
 //Los valores se toman desde el input del html, por medio del id asignado.
 
+const getDatosBoton = document.querySelector(".button");
 
-function botonContinuar() {
-   cantCajas = document.getElementById("caja").value;
-   pesoCaja = document.getElementById("peso").value;
-   anchoCaja = document.getElementById("ancho-caja").value;
-   largoCaja = document.getElementById("largo-caja").value;
-   altoCaja = document.getElementById("alto-caja").value;
+getDatosBoton.addEventListener("click", getDatos);
 
-   console.log({cantCajas, pesoCaja, anchoCaja, largoCaja, altoCaja});
+function getDatos() {
+  cantCajas = document.getElementById("caja").value;
+  pesoCaja = document.getElementById("peso").value;
+  anchoCaja = document.getElementById("ancho-caja").value;
+  largoCaja = document.getElementById("largo-caja").value;
+  altoCaja = document.getElementById("alto-caja").value;
+
+  console.log({ cantCajas, pesoCaja, anchoCaja, largoCaja, altoCaja });
+
+   return cantCajas,pesoCaja,anchoCaja,largoCaja,altoCaja;
 };
 
-botonContinuar();
-
-const getDatos = document.querySelector(".button");
-
-getDatos.addEventListener("click", botonContinuar);
-
+getDatos();
 
 //! CALCULAR PESO VOLUMEN
 //Resultado de multipliar las dimensiones de la unidad en mts
 // (ancho X largo X alto de la caja) X factor de equivalencia
-// El factor de equivalencia = 400kg / m3 
+// El factor de equivalencia = 400kg / m3
 
-function pesoTotalVolumen(anchoCaja,largoCaja,altoCaja) {
-   let factorEquivalencia = 400;
-   pesoVol = (((anchoCaja/100) * (largoCaja/100) * (altoCaja/100)) * factorEquivalencia);
-   pesoVol = Math.round(pesoVol);
-   console.log("1. peso volumen caja= " + Math.round(pesoVol));
-   
-   return pesoVol;
+function pesoTotalVolumen(anchoCaja, largoCaja, altoCaja) {
+  console.log(cantCajas);
 
-};
+  let factorEquivalencia = 400;
+  pesoVol =
+    (anchoCaja / 100) *
+    (largoCaja / 100) *
+    (altoCaja / 100) *
+    factorEquivalencia;
+  pesoVol = Math.round(pesoVol);
+  console.log("1. peso volumen caja= " + Math.round(pesoVol));
+
+  return pesoVol;
+}
 
 pesoTotalVolumen(anchoCaja,largoCaja,altoCaja);
 
-//!SELECCIONAR PESO 
+//!SELECCIONAR PESO
 
-function pesoFlete (pesoCaja,pesoVol,cantCajas) {
-   console.log("2. Entro peso caja = " + pesoCaja);
-   console.log("3. Entro peso volumen = " + pesoVol);
-   if (pesoCaja > pesoVol) {
-      pesoTotalFlete = pesoCaja * cantCajas;
-   } else  {
-      pesoTotalFlete = pesoVol * cantCajas;
-   } console.log("4. peso flete= " + pesoTotalFlete);
+function pesoFlete(pesoCaja, pesoVol, cantCajas) {
+  console.log("2. Entro peso caja = " + pesoCaja);
+  console.log("3. Entro peso volumen = " + pesoVol);
+  if (pesoCaja > pesoVol) {
+    pesoTotalFlete = pesoCaja * cantCajas;
+  } else {
+    pesoTotalFlete = pesoVol * cantCajas;
+  }
+  console.log("4. peso flete= " + pesoTotalFlete);
 
-   return pesoTotalFlete;
-
-};
+  return pesoTotalFlete;
+}
 
 pesoFlete (pesoCaja,pesoVol,cantCajas);
-
-
-
-
