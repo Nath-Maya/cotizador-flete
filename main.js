38f5af83555d1ec90e89d17fe1ffffa00c35202c
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-06-16 12:43:55 +0000

    Funcion getDatos para capturar valores de los input

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 //*Variables
 
 
-let cantCajas = 5;
-let pesoCaja = 25;
-let anchoCaja = 54; 
-let largoCaja = 37; 
-let altoCaja = 25;
+let cantCajas = 0;
+let pesoCaja = 0;
+let anchoCaja = 0; 
+let largoCaja = 0; 
+let altoCaja = 0;
 let pesoVol = 0;
 let pesoTotalFlete = 0;
 
@@ -16,19 +16,22 @@ let pesoTotalFlete = 0;
 //Datos que registra el usuario necesarios para hacer los calculos de peso neto y peso volumen.
 //Los valores se toman desde el input del html, por medio del id asignado.
 
-/*
-function getDatos() {
-  cantCajas = document.getElementById("caja").value;
-  pesoCaja = document.getElementById("peso").value;
-  anchoCaja = document.getElementById("ancho-caja").value;
-  largoCaja = document.getElementById("largo-caja").value;
-  altoCaja = document.getElementById("alto-caja").value;
 
-  return cantCajas, pesoCaja, anchoCaja, largoCaja, altoCaja;
-}
+function botonContinuar() {
+   cantCajas = document.getElementById("caja").value;
+   pesoCaja = document.getElementById("peso").value;
+   anchoCaja = document.getElementById("ancho-caja").value;
+   largoCaja = document.getElementById("largo-caja").value;
+   altoCaja = document.getElementById("alto-caja").value;
 
-getDatos();
-*/
+   console.log({cantCajas, pesoCaja, anchoCaja, largoCaja, altoCaja});
+};
+
+botonContinuar();
+
+const getDatos = document.querySelector(".button");
+
+getDatos.addEventListener("click", botonContinuar);
 
 
 //! CALCULAR PESO VOLUMEN
@@ -64,4 +67,7 @@ function pesoFlete (pesoCaja,pesoVol,cantCajas) {
 };
 
 pesoFlete (pesoCaja,pesoVol,cantCajas);
+
+
+
 
